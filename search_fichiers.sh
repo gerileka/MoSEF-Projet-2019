a56d7163e860e79780f30e8977dc4e029df5af06
gerileka <gerileka@hotmail.com>
2019-11-09 18:47:59 +0100

    Ajoute du Filter2

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -4,8 +4,10 @@ echo "Welcome to my project sir,what's my repository?"
 read -p "Write the prefered repository: "  repos
 cat $repos
 
-read -p "Please enter your filtering features:" fil1
-locate $repos/$fil1
+read -p "Please enter your filtering features:" fil1 fil2
+locate $repos/$fil1 | grep -i $fil2
+
+
 
 
 
