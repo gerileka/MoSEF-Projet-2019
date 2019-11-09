541e10dbcbfa78fdc20a2814312af57944cb5c79
gerileka <gerileka@hotmail.com>
2019-11-09 18:09:42 +0100

    Ajoute du Filter1

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "This file is opened $(date)"
 echo "Welcome to my project sir,what's my repository?"
-read -p 'Write the prefered repository: ' repos
+read -p "Write the prefered repository: "  repos
 cat $repos
+
+read -p "Please enter your filtering features:" fil1
+locate $repos/$fil1
+
+
+
 
