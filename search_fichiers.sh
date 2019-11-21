2c215d81c31468ef2a64c45f62d98fa6ee8d2a00
gerileka <56592646+gerileka@users.noreply.github.com>
2019-11-21 20:41:46 +0100

    Update search_fichiers.sh

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -2,7 +2,7 @@
 echo "This file is opened $(date)"
 echo "Welcome to my project sir,what's my repository?"
 read -p "Write the prefered repository: "  repos
-cat $repos
+cd /home/$USERNAME/$repos
 
 read -p "Please enter your filtering features:" fil1 fil2
 locate $repos/$fil1 | grep -i $fil2
